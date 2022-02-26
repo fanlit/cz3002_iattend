b412ee1ee5603da1ed1fb5fe8fd0251a0f5b3311
fanlit <fanlit1998@gmail.com>
2022-02-26 16:35:26 +0800

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -48,20 +48,9 @@ class StartUpPage extends StatefulWidget {
 }
 
 class _StartUpPageState extends State<StartUpPage> {
-  double _counter = 0;
   double fonttitle = 50;
   double fontregular = 20;
   double iconsize = 3.5;
-  // void _incrementCounter() {
-  //   setState(() {
-  //     // This call to setState tells the Flutter framework that something has
-  //     // changed in this State, which causes it to rerun the build method below
-  //     // so that the display can reflect the updated values. If we changed
-  //     // _counter without calling setState(), then the build method would not be
-  //     // called again, and so nothing would appear to happen.
-  //     _counter++;
-  //   });
-  // }
 
   @override
   Widget build(BuildContext context) {
