0cce8422ed0020255695f0b59999d674507120d6
Okka1998 <okka001@e.ntu.edu.sg>
2022-03-18 01:23:28 +0800

    Added functions to models

--- lib/Services/DatabaseServices/UserList.dart
+++ lib/Services/DatabaseServices/UserList.dart
@@ -3,7 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 
 import '../../Models/user.dart';
-
+/*
 class UserList extends StatefulWidget {
   const UserList({Key? key}) : super(key: key);
 
@@ -11,7 +11,7 @@ class UserList extends StatefulWidget {
   _UserListState createState() => _UserListState();
 }
 
-/*
+
 class _UserListState extends State<UserList> {
   @override
   Widget build(BuildContext context) {
